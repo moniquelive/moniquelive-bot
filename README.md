354a82e74b3c92759be8364e16d3e523bb0a754f
Cyber Oliveira <lccro@msn.com>
2021-03-16 00:59:27 -0300

    extrai comandos para .json e faz parse com text/Template

--- README.md
+++ README.md
@@ -7,12 +7,15 @@
 - limits (whisper, etc): https://dev.twitch.tv/limit-increase/
 
 # Brainstorm
-   
-- comandos em _en_ e _pt-br_
-- timers (alonga, hidrata, etc.)
+
+- [ ] hot reload config
+- [ ] comandos em _en_ e _pt-br_
+- [ ] timers (alonga, hidrata, etc.)
 
 # DONE
-- ✅ /slow 1
-- ✅ /uniquechat
-- ✅ /me bot responses 
-- ✅ bookmarks (!youtube/!yt,!cybervox/!vox,!github/!gh)
+- [x] /slow 1
+- [x] /uniquechat
+- [x] /me bot responses
+- [x] bookmarks (!youtube/!yt,!cybervox/!vox,!github/!gh)
+- [x] extrair comandos para .json
+- [x] usar template para fazer comandos dinamicos
--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bytes"
 	_ "embed"
+	"encoding/json"
 	"log"
-	"strconv"
+	"os"
 	"strings"
+	"text/template"
 
 	"github.com/gempir/go-twitch-irc/v2"
 )
@@ -18,74 +21,41 @@ const (
 	moniqueId = "4930146"
 )
 
+var commands []struct {
+	Actions  []string `json:"actions"`
+	Color    string   `json:"color"`
+	Response string   `json:"response"`
+}
+
+func init() {
+	file, err := os.Open("commands.json")
+	if err != nil {
+		log.Fatalln("erro ao abrir commands.json:", err)
+	}
+	defer file.Close()
+
+	if err := json.NewDecoder(file).Decode(&commands); err != nil {
+		log.Fatalln("erro ao parsear commands.json:", err)
+	}
+}
+
 func main() {
 	roster := map[string]bool{}
 	client := twitch.NewClient(username, oauth)
 
 	client.OnConnect(func() {
 		log.Println("OnConnect") // OnConnect attach callback to when a connection has been established
-		//var colors = [...]string{
-		//	"Blue",
-		//	"Coral",
-		//	"DodgerBlue",
-		//	"SpringGreen",
-		//	"YellowGreen",
-		//	"Green",
-		//	"OrangeRed",
-		//	"Red",
-		//	"GoldenRod",
-		//	"HotPink",
-		//	"CadetBlue",
-		//	"SeaGreen",
-		//	"Chocolate",
-		//	"BlueViolet",
-		//	"Firebrick",
-		//}
-		//for _, color := range colors {
-		//	client.Say(channel, "/color "+color)
-		//	client.Say(channel, "/me "+color)
-		//}
+		//rainbow(client)
 		client.Say(channel, "/color seagreen")
 		client.Say(channel, "/me Tô na área!")
 		client.Say(channel, "/slow 1")
 		client.Say(channel, "/uniquechat")
 	})
 
-	client.OnWhisperMessage(func(message twitch.WhisperMessage) {
-		log.Println("OnWhisperMessage: ", message) // OnWhisperMessage attach to new whisper
-	})
-
-	client.OnPrivateMessage(func(message twitch.PrivateMessage) {
-		log.Println("OnPrivateMessage: ", message) // OnPrivateMessage attach to new standard chat messages
-	})
-
-	client.OnClearChatMessage(func(message twitch.ClearChatMessage) {
-		log.Println("OnClearChatMessage: ", message) // OnClearChatMessage attach to new messages such as timeouts
-	})
-
-	//client.OnClearMessage(func(message twitch.ClearMessage) {
-	//	log.Println("OnClearMessage: ", message) // OnClearMessage attach when a single message is deleted
-	//})
-
-	//client.OnRoomStateMessage(func(message twitch.RoomStateMessage) {
-	//	log.Println("OnRoomStateMessage: ", message) // OnRoomStateMessage attach to new messages such as submode enabled
-	//})
-
-	client.OnUserNoticeMessage(func(message twitch.UserNoticeMessage) {
-		log.Println("OnUserNoticeMessage: ", message) // OnUserNoticeMessage attach to new usernotice message such as sub, resub, and raids
-	})
-
-	//client.OnUserStateMessage(func(message twitch.UserStateMessage) {
-	//	log.Println("OnUserStateMessage: ", message) // OnUserStateMessage attach to new userstate
-	//})
-
-	//client.OnGlobalUserStateMessage(func(message twitch.GlobalUserStateMessage) {
-	//	log.Println("OnGlobalUserStateMessage: ", message) // OnGlobalUserStateMessage attach to new global user state
-	//})
-
-	//client.OnNoticeMessage(func(message twitch.NoticeMessage) {
-	//	log.Println("OnNoticeMessage: ", message) // OnNoticeMessage attach to new notice message such as hosts
-	//})
+	client.OnWhisperMessage(func(message twitch.WhisperMessage) { log.Println("OnWhisperMessage: ", message) })          // OnWhisperMessage attach to new whisper
+	client.OnPrivateMessage(func(message twitch.PrivateMessage) { log.Println("OnPrivateMessage: ", message) })          // OnPrivateMessage attach to new standard chat messages
+	client.OnClearChatMessage(func(message twitch.ClearChatMessage) { log.Println("OnClearChatMessage: ", message) })    // OnClearChatMessage attach to new messages such as timeouts
+	client.OnUserNoticeMessage(func(message twitch.UserNoticeMessage) { log.Println("OnUserNoticeMessage: ", message) }) // OnUserNoticeMessage attach to new usernotice message such as sub, resub, and raids
 
 	client.OnUserJoinMessage(func(message twitch.UserJoinMessage) {
 		roster[message.User] = true
@@ -95,64 +65,42 @@ func main() {
 		delete(roster, message.User)
 	})
 
-	//client.OnReconnectMessage(func(message twitch.ReconnectMessage) {
-	//	log.Println("OnReconnectMessage: ", message) // OnReconnectMessage attaches that is triggered whenever the twitch servers tell us to reconnect
-	//})
-
 	client.OnNamesMessage(func(message twitch.NamesMessage) {
 		for _, user := range message.Users {
 			roster[user] = true
 		}
 	})
 
-	//// OnPingMessage attaches to PING message
-	//client.OnPingMessage(func(message twitch.PingMessage) {
-	//	log.Println("OnPingMessage: ", message)
-	//})
-	//
-	//// OnPongMessage attaches to PONG message
-	//client.OnPongMessage(func(message twitch.PongMessage) {
-	//	log.Println("OnPongMessage: ", message)
-	//})
-	//
-	//// OnUnsetMessage attaches to message types we currently don't support
-	//client.OnUnsetMessage(func(message twitch.RawMessage) {
-	//	log.Println("OnUnsetMessage: ", message)
-	//})
-	//
-	//// OnPingSent attaches that's called whenever the client sends out a ping message
-	//client.OnPingSent(func() {
-	//	log.Println("OnPingSent")
-	//})
-
 	client.OnPrivateMessage(func(message twitch.PrivateMessage) {
 		log.Printf("%s (%v): %s\n", message.User.DisplayName, message.User.ID, message.Message)
 		//if message.User.ID == moniqueId {
 		//}
-		if strings.HasPrefix(message.Message, "!roster") {
-			client.Say(channel, "/color blue")
-			client.Say(channel, "/me "+strconv.Itoa(len(roster)))
-			//var users []string
-			//for user := range roster {
-			//	users = append(users, user)
-			//}
-			//msg := strings.Join(users, " ")
-			//log.Println(">>>", msg)
-		} else if strings.HasPrefix(message.Message, "!youtube") || strings.HasPrefix(message.Message, "!yt") {
-			client.Say(channel, "/color yellowgreen")
-			client.Say(channel, "/me playlist com os vídeos passados: https://www.youtube.com/playlist?list=PLyR9xTdgYCd9n4V5EK86aEqMd59QZeDD9")
-		} else if strings.HasPrefix(message.Message, "!cybervox") || strings.HasPrefix(message.Message, "!vox") {
-			client.Say(channel, "/color yellowgreen")
-			client.Say(channel, "/me clique neste link no seu celular para falar com o robô: https://wa.me/5521982820415?text=%23mandaaudios&lang=pt_br")
-		} else if strings.HasPrefix(message.Message, "!github") || strings.HasPrefix(message.Message, "!gh") {
-			client.Say(channel, "/color yellowgreen")
-			client.Say(channel, "/me clique neste link para ver nossos projetinhos: https://github.com/moniquelive")
-		} else if strings.HasPrefix(message.Message, "!meme") {
-			client.Say(channel, "/color yellowgreen")
-			client.Say(channel, "/me clique neste link para ver nosso gerador de memes: https://meme.monique.dev")
-		} else if strings.HasPrefix(message.Message, "!") {
+		//var users []string
+		//for user := range roster {
+		//	users = append(users, user)
+		//}
+		//msg := strings.Join(users, " ")
+		//log.Println(">>>", msg)
+		for _, command := range commands {
+			matches := false
+			for _, action := range command.Actions {
+				matches = matches || strings.HasPrefix(message.Message, action)
+			}
+			if matches {
+				response, err := parse(roster, command.Response)
+				if err != nil {
+					log.Printf("erro ao parsear command %q: %v", command.Response, err)
+					return
+				}
+				client.Say(channel, "/color "+command.Color)
+				client.Say(channel, response)
+				return
+			}
+		}
+
+		if strings.HasPrefix(message.Message, "!") {
 			client.Say(channel, "/color firebrick")
-			client.Say(channel, "/me do que que você está falando?!")
+			client.Say(channel, "/me ⁉ do que que você está falando?!")
 		}
 	})
 
@@ -161,5 +109,46 @@ func main() {
 	err := client.Connect()
 	if err != nil {
 		panic(err)
+	}
+}
+
+func parse(roster map[string]bool, response string) (string, error) {
+	var vars struct {
+		Roster map[string]bool
+	}
+	vars.Roster = roster
+	tmpl, err := template.New("command").Parse(response)
+	if err != nil {
+		return "", err
+	}
+	parsed := bytes.NewBufferString("")
+	err = tmpl.Execute(parsed, vars)
+	if err != nil {
+		return "", err
+	}
+	return parsed.String(), nil
+}
+
+func rainbow(client *twitch.Client) {
+	var colors = [...]string{
+		"Blue",
+		"Coral",
+		"DodgerBlue",
+		"SpringGreen",
+		"YellowGreen",
+		"Green",
+		"OrangeRed",
+		"Red",
+		"GoldenRod",
+		"HotPink",
+		"CadetBlue",
+		"SeaGreen",
+		"Chocolate",
+		"BlueViolet",
+		"Firebrick",
+	}
+	for _, color := range colors {
+		client.Say(channel, "/color "+color)
+		client.Say(channel, "/me "+color)
 	}
 }
